a9f72d17f1f319a6d9198352f09c9987cffc964f
richplastow <rich@richplastow.com>
2016-07-14 10:04:51 +0100

    0.0.9 with typings for jQuery `mmenu` and `tinyscrollbar`

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="globals/jquery.mmenu/index.d.ts" />
+/// <reference path="globals/jquery.tinyscrollbar/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/slick-carousel/slick-carousel.d.ts" />
